d35bc02812b2118791a229177fae06f79e80f144
Noor Shaikh <developernoorshaikh00@gmail.com>
2025-01-03 14:24:52 +0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+# Variables
+spacer = "===================="
+message1 = "\n" + spacer + "\nHello World\n" + spacer + "\n"
+displayMessage = ("This Is My First Program In Python")
+message2 = "\n" + spacer + str(displayMessage) + spacer + "\n"
+
+# Print Message Variables Out
+print(message1)
+print(message2)
